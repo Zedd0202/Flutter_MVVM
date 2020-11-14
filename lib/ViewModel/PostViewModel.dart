9726f5f93ba9b140810fc2fd829737374eeadf99
Zedd0202 <ch41150@hanmail.net>
2020-11-14 17:31:08 +0900

    리스트뷰 구현 완료

--- lib/ViewModel/PostViewModel.dart
+++ lib/ViewModel/PostViewModel.dart
@@ -1,23 +1,19 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_app_mvvm/Model/Post.dart';
 
-class PostViewModel extends ChangeNotifier {
-  List<Post> lists = List<Post>();
+class PostViewModel {
+  String url = 'https://jsonplaceholder.typicode.com/posts';
 
   Future<List<Post>> fetchPost() async {
-    final response =
-    await http.get('https://jsonplaceholder.typicode.com/posts');
+    final response = await http.get(this.url);
     if (response.statusCode == 200) {
       // 만약 서버가 OK 응답을 반환하면, JSON을 파싱합니다.
       List responseJson = json.decode(response.body);
       return responseJson.map((post) => new Post.fromJson(post)).toList();
-      //notifyListeners();
     } else {
       // 만약 응답이 OK가 아니면, 에러를 던집니다.
       throw Exception('Failed to load post');
     }
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,53 @@ import 'package:flutter_app_mvvm/ViewModel/PostViewModel.dart';
 import 'package:flutter_app_mvvm/Model/Post.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('Fetch Data Example'),
-      ),
-      body: MyApp())));
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Fetch Data Example'),
+          ),
+          body: ListContainer())));
 }
 
-class MyApp extends StatefulWidget {
-
+class ListContainer extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  _ListContainerState createState() => _ListContainerState();
 }
 
-class _MyAppState extends State<MyApp> {
-
-  PostViewModel viewModel = PostViewModel();
+class _ListContainerState extends State<ListContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: FutureBuilder<List<Post>>(
-    future: this.viewModel.fetchPost(),
-    builder: (context, snapshot) {
-      if (snapshot.hasData) {
-        return ListView.builder(
-          itemCount: snapshot.data.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${snapshot.data[index].title}'),
-            );
-          },
-        );
-        return Text(snapshot.data.first.body);
-      } else if (snapshot.hasError) {
-        return Text("${snapshot.error}");
-      }
-      // 기본적으로 로딩 Spinner를 보여줍니다.
-      return CircularProgressIndicator();
-    },)
+    return Center(
+        child: Container(
+            child: ListWidget()));
+  }
+}
+
+class ListWidget extends StatelessWidget {
+
+  final PostViewModel viewModel = PostViewModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Post>>(
+      future: this.viewModel.fetchPost(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('${snapshot.data[index].title}'),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // 기본적으로 로딩 Spinner를 보여줍니다.
+        return CircularProgressIndicator();
+      },
     );
   }
 }
